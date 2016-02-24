668134d3199ffcf6a4627cc04197ee1c3c068197
landlessness <brian@landlessness.net>
2016-02-24 15:30:06 -0500

    added bangalore

---
+++ bangalore.js
@@ -0,0 +1,17 @@
+var zetta = require('zetta');
+var LED = require('zetta-led-mock-driver');
+var Photocell = require('zetta-photocell-mock-driver');
+
+var duskToDawnLight = require('./apps/dusk_to_dawn_light');
+
+var PORT = 1339;
+
+zetta()
+  .name('bangalore')
+  .use(LED)
+  .use(Photocell)
+  .use(duskToDawnLight)
+  .link('http://hello.zettaapi.org/')
+  .listen(PORT, function(){
+     console.log('Zetta is running at http://127.0.0.1:', PORT);
+});

ef3750f3c34ed77cb0274521098321003ef214c1
landlessness <brian@landlessness.net>
2016-02-24 17:13:09 -0500

    link to demo

---
+++ denver.1344.js
@@ -0,0 +1,17 @@
+var zetta = require('zetta');
+var LED = require('zetta-led-mock-driver');
+var Photocell = require('zetta-photocell-mock-driver');
+
+var duskToDawnLight = require('./apps/dusk_to_dawn_light');
+
+var PORT = 1344;
+
+zetta()
+  .name('denver')
+  .use(LED)
+  .use(Photocell)
+  .use(duskToDawnLight)
+  .link('http://demo.zettaapi.org/')
+  .listen(PORT, function(){
+     console.log('Zetta is running at http://127.0.0.1:', PORT);
+});

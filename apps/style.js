661e19821b0be8f1f17bcb16f53b0653def3f66c
landlessness <brian@landlessness.net>
2016-03-01 23:48:56 -0500

    merging style info

--- apps/style.js
+++ apps/style.js
@@ -1,3 +1,5 @@
+var extend = require('node.extend');
+
 var DOOR_CLOSED_IMAGE_URL = 'https://cdn0.iconfinder.com/data/icons/mobile-development-svg-icons/60/closed_door-512.png';
 var DOOR_OPENED_IMAGE_URL = 'https://cdn0.iconfinder.com/data/icons/mobile-development-svg-icons/60/open_door-512.png';
 
@@ -6,16 +8,17 @@ module.exports = function(server) {
   server.observe([doorSensorQuery], function(doorSensor){
     // add property to track style
     doorSensor.style = {};
-    doorSensor.style.stateImage = DOOR_CLOSED_IMAGE_URL;
     // wrap it in a WebSockets monitor
     doorSensor._initMonitor('style');
     doorSensor._monitors.push('style');
+    // set the default image
+    doorSensor.style.stateImage = DOOR_CLOSED_IMAGE_URL;
     // change the state image when door changes state
     doorSensor.on('force-mock-close', function(s) {
-      doorSensor.style = {stateImage: DOOR_CLOSED_IMAGE_URL};
+      doorSensor.style = extend(doorSensor.style, {stateImage: DOOR_CLOSED_IMAGE_URL});
     });
     doorSensor.on('force-mock-open', function(s) {
-      doorSensor.style = {stateImage: DOOR_OPENED_IMAGE_URL};
+      doorSensor.style = extend(doorSensor.style, {stateImage: DOOR_OPENED_IMAGE_URL});
     });
    }
   );

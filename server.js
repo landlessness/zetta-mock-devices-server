ae4ede131b1f05fe81c685793d55618523ed472e
landlessness <brian@landlessness.net>
2016-03-25 15:02:04 -0400

    more sample server commands

--- server.js
+++ server.js
@@ -79,10 +79,13 @@ zetta()
 // pm2 start server.js  --name stage.bangalore.1371 -- -s bangalore -p 1371 -l http://stage.zettaapi.org -f 193:80:32:#C15020 -c http://www.zettaapi.org/public/demo/bangalore.jpg
 
 // DENVER
-// pm2 start server.js  --name stage.denver.1372 -- -s denver -p 1372 -l http://stage.zettaapi.org -c http://www.zettaapi.org/public/demo/denver.jpg
+// pm2 start server.js  --name stage.denver.1372 -- -s denver -p 1372 -l http://stage.zettaapi.org -f 248:86:17:#F85611 -b 0:30:72:#001E48 -c http://www.zettaapi.org/public/demo/denver.jpg
 
 // DETROIT
-// pm2 start server.js  --name stage.detroit.1373 -- -s detroit -p 1373 -l http://stage.zettaapi.org -f 255:202:0:#FFCA00 -b 0:35:80:#002350 -c http://www.zettaapi.org/public/demo/detroit.jpg
+// pm2 start server.js  --name stage.detroit.1373 -- -s detroit -p 1373 -l http://stage.zettaapi.org -f 0:80:154:#00509A -b 171:182:191:#ABB6BF -c http://www.zettaapi.org/public/demo/detroit.jpg
+
+// ANN ARBOR
+// pm2 start server.js  --name stage.annarbor.1373 -- -s annarbor -p 1373 -l http://stage.zettaapi.org -f 255:191:0:#FFBF00 -b 0:30:60:#001E3C -c http://www.zettaapi.org/public/demo/annarbor.jpg
 
 // DUBLIN
 // pm2 start server.js  --name stage.dublin.1374 -- -s dublin -l http://stage.zettaapi.org -p 1374 -f 255:255:255:#FFFFFF -b 0:155:72:#009B48 -c http://www.zettaapi.org/public/demo/dublin.jpg
@@ -91,7 +94,10 @@ zetta()
 // pm2 start server.js  --name stage.neworleans.1375 -- -s neworleans -l http://stage.zettaapi.org -p 1375 -f 98:47:187:#622FBB -c http://www.zettaapi.org/public/demo/neworleans.jpg
 
 // SAN FRANCISCO
-// pm2 start server.js  --name stage.sanfrancisco.1376 -- -s sanfrancisco -l http://stage.zettaapi.org -p 1376 -f 255:215:0:#FFD700  -c http://www.zettaapi.org/public/demo/sanfrancisco.jpg
+// pm2 start server.js  --name stage.sanfrancisco.1376 -- -s sanfrancisco -l http://stage.zettaapi.org -p 1376 -f 182:136:56:#B68838 -b 194:0:18:#C20012 -c http://www.zettaapi.org/public/demo/sanfrancisco.jpg
 
 // SINGAPORE
 // pm2 start server.js  --name stage.singapore.1377 -- -s singapore -l http://stage.zettaapi.org -p 1377 -c http://www.zettaapi.org/public/demo/singapore.jpg
+
+// ANN ARBOR
+// pm2 start server.js  --name stage.annarbor.1378 -- -s annarbor -p 1378 -l http://stage.zettaapi.org -f 255:191:0:#FFBF00 -b 0:30:60:#001E3C -c http://www.zettaapi.org/public/demo/annarbor.jpg
